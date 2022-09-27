bdb98ac0e5343e55595952b206769932a207c01d
ennzoski <e.toledo.cardenas@gmail.com>
2022-09-27 00:27:45 -0300

    actualización caja de preguntas

--- JS/script.js
+++ JS/script.js
@@ -1,84 +1,35 @@
-/* COMENTARIO
+/* Colores de calcetines disponibles */
 
-let nombreProductoA = 'Mesa'
-let precioProductoA = 100
-let stockProductoA = 10
+let nombreProductoA = 'Blanco'
+let precioProductoA = 5000
+let stockProductoA = 25
 
 let nombreProductoB = 'Verde'
-let precioProductoB = 10
-let stockProductoB = 20
+let precioProductoB = 4500
+let stockProductoB = 30
 
 let nombreProductoC = 'Azul'
-let precioProductoC = 20
-let stockProductoC = 50
+let precioProductoC = 6000
+let stockProductoC = 15
 
-//let compra = prompt("Ingrese SI si quiere comprar: " + nombreProductoA + " y " + nombreProductoB)
-
-let productoCompra = prompt("Ingrese que producto quiere comprar: \n - Mesa\n - Verde\n - Azul")
+let productoCompra = prompt("Ingrese que color de calcetin quiere comprar: \n - Blanco\n - Verde\n - Azul")
 let precioTotal = 0
 
-if(productoCompra.toUpperCase() == 'MESA'){
-    let cantidadProductoMesa = prompt("ingrese que cantidad de " + nombreProductoA + " desea comprar:")
-    if(cantidadProductoMesa <= stockProductoA){
-        precioTotal = cantidadProductoMesa * precioProductoA
+ if(productoCompra.toUpperCase() == 'Blanco'){
+    let cantidadProductoBlanco = prompt("ingrese cuantos pares de calcetin de color " + nombreProductoA + " desea comprar:")
+    if(cantidadProductoBlanco <= stockProductoA){
+        precioTotal = cantidadProductoBlanco * precioProductoA
     }
     else{
         alert("Actualmente tenemos " + stockProductoA + " unidades de este producto")
     }
 }
 else if(productoCompra == 'Verde'){
-    let cantidadProductoVerde = prompt("ingrese que cantidad de " + nombreProductoB + " desea comprar:")
+    let cantidadProductoVerde = prompt("ingrese cuantos pares de calcetin de color " + nombreProductoB + " desea comprar:")
     precioTotal = cantidadProductoVerde * precioProductoB
 }
 else if(productoCompra == 'Azul'){
-    let cantidadProductoAzul = prompt("ingrese que cantidad de " + nombreProductoC + " desea comprar:")
-    precioTotal = cantidadProductoAzul * precioProductoC
-}
-else{
-    alert("No tenemos ese producto a la venta")
-}
-
-if(precioTotal != 0){
-    alert("El precio total es: " + precioTotal)
-}
-
-else{
-    alert("Gracias por su visita")
-}
-*/
-
-let nombreProductoA = 'Rojo'
-let precioProductoA = 100
-let stockProductoA = 10
-
-let nombreProductoB = 'Verde'
-let precioProductoB = 10
-let stockProductoB = 20
-
-let nombreProductoC = 'Azul'
-let precioProductoC = 20
-let stockProductoC = 50
-
-//let compra = prompt("Ingrese SI si quiere comprar: " + nombreProductoA + " y " + nombreProductoB)
-
-let productoCompra = prompt("Ingrese que producto quiere comprar: \n - Rojo\n - Verde\n - Azul")
-let precioTotal = 0
-
-if(productoCompra.toUpperCase() == 'Rojo'){
-    let cantidadProductoRojo = prompt("ingrese que cantidad de " + nombreProductoA + " desea comprar:")
-    if(cantidadProductoRojo <= stockProductoA){
-        precioTotal = cantidadProductoRojo * precioProductoA
-    }
-    else{
-        alert("Actualmente tenemos " + stockProductoA + " unidades de este producto")
-    }
-}
-else if(productoCompra == 'Verde'){
-    let cantidadProductoVerde = prompt("ingrese que cantidad de " + nombreProductoB + " desea comprar:")
-    precioTotal = cantidadProductoVerde * precioProductoB
-}
-else if(productoCompra == 'Azul'){
-    let cantidadProductoAzul = prompt("ingrese que cantidad de " + nombreProductoC + " desea comprar:")
+    let cantidadProductoAzul = prompt("ingrese cuantos pares de calcetin de color " + nombreProductoC + " desea comprar:")
     precioTotal = cantidadProductoAzul * precioProductoC
 }
 else{
